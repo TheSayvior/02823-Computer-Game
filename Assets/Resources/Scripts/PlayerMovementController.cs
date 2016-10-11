8c56d17a2c812c8c870a90453ad7cde1b6c313c6
DESKTOP-L76KAL5\Rasmus Jensen <nevatarion@hotmail.com>
2016-10-11 16:07:43 +0200

    Added first demo scene for rope

---
+++ Assets/Resources/Scripts/PlayerMovementController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementController : MonoBehaviour {
+
+    bool Swinging;
+
+    Rigidbody2D _playerRB2D;
+
+	// Use this for initialization
+	void Start () {
+        _playerRB2D = this.gameObject.GetComponent<Rigidbody2D>();
+        Swinging = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        if(Swinging == true)
+        {
+            return;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            if (_playerRB2D.velocity.x > -5)
+                _playerRB2D.velocity = _playerRB2D.velocity + new Vector2(-1, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            if (_playerRB2D.velocity.x < 5)
+                _playerRB2D.velocity = _playerRB2D.velocity + new Vector2(1, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            if (_playerRB2D.velocity.y < 5)
+                _playerRB2D.velocity = _playerRB2D.velocity + new Vector2(0, 1);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/RopeController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RopeController : MonoBehaviour {
+
+    HingeJoint2D _rope;
+	// Use this for initialization
+	void Start () {
+        _rope = this.gameObject.GetComponent<HingeJoint2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            FindRopeConnection();
+        }
+
+
+       // _rope.anchor
+	}
+
+    private void FindRopeConnection()
+    {
+        throw new NotImplementedException();
+    }
+}
