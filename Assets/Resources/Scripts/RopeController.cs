dc55734094b3c581548c304dc7a7bf9a6a1a255c
DESKTOP-L76KAL5\Rasmus Jensen <nevatarion@hotmail.com>
2016-10-25 14:43:37 +0200

    Rope attachment

--- Assets/Resources/Scripts/RopeController.cs
+++ Assets/Resources/Scripts/RopeController.cs
@@ -4,11 +4,13 @@ using System;
 
 public class RopeController : MonoBehaviour {
 
+    public GameObject Player;
+
     HingeJoint2D _rope;
+
 	// Use this for initialization
 	void Start () {
         _rope = this.gameObject.GetComponent<HingeJoint2D>();
-
     }
 	
 	// Update is called once per frame
@@ -24,6 +26,19 @@ public class RopeController : MonoBehaviour {
 	}
 
     private void FindRopeConnection()
+    {
+        if (_rope.isActiveAndEnabled == true)
+        {
+            _rope.enabled = false;
+        }
+        else if (_rope.isActiveAndEnabled == false)
+        {
+            _rope.enabled = true;
+            moveAnchor();
+        }
+    }
+
+    private void moveAnchor()
     {
         throw new NotImplementedException();
     }
