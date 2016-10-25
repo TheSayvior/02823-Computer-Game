e6a0d2c338bd91ae858029bfc0225a89f7015ce4
Kevin Kuek <kevin.kuek.95@gmail.com>
2016-10-25 15:30:31 +0200

    kevin test scripts

---
+++ Assets/Resources/Scripts/deployRope.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deployRope : MonoBehaviour {
+
+	public GameObject rope;
+
+	private bool isConnected;
+
+	// Use this for initialization
+	void Start () {
+		rope.SetActive (false);
+		isConnected = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (!isConnected) {
+				rope.transform.position = Input.mousePosition;
+				rope.SetActive (true);
+				isConnected = true;
+			} else {
+				rope.SetActive (false);
+				isConnected = false;
+			}
+		}
+	}
+}
