e7cf012a2ea6c894f1327206e77578644aefd6c0
TheSayvior <Nevatarion@hotmail.com>
2016-12-14 00:55:13 +0100

    added the posibility for sound and fixed the ending picture

---
+++ Assets/SoundManaging.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManaging : MonoBehaviour {
+
+    GameObject _player;
+
+    AudioSource Flame;
+
+	// Use this for initialization
+	void Start () {
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+        this.transform.parent = _player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
